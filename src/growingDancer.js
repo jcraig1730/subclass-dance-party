9c58e19ae85611981149c5ca9d390eb8e047fb7f
jcraig1730 <jcraig1730@gmail.com>
2019-07-22 21:45:24 -0700

    added squirtle to the dance party

--- src/growingDancer.js
+++ src/growingDancer.js
@@ -2,7 +2,7 @@ var makeGrowingDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps)
   this.height = 40;
   this.width = 40;
-  this.$node.css("background-image", 'url("https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png")';
+  this.$node.css("background-image", 'url("https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png")');
 }
 
 makeGrowingDancer.prototype = Object.create(makeDancer.prototype);
@@ -10,20 +10,32 @@ makeGrowingDancer.prototype.constructor = makeGrowingDancer;
 
 makeGrowingDancer.prototype.step = function(timeBetweenSteps) {
   setTimeout(()=> {
-    this.width += 20;
-    this.height += 20;
+    this.width += 10;
+    this.height += 10;
     this.setPosition();
   }, 100);
   setTimeout(()=> {
-    this.width -= 20;
-    this.height -= 20;
+    this.width += 10;
+    this.height += 10;
     this.setPosition();
   }, 200);
+  setTimeout(()=> {
+    this.width -= 10;
+    this.height -= 10;
+    this.setPosition();
+  }, 300);
+  setTimeout(()=> {
+    this.width -= 10;
+    this.height -= 10;
+    this.setPosition();
+  }, 400);
   setTimeout(() => { this.step(timeBetweenSteps) }, timeBetweenSteps);
 }
 
 makeGrowingDancer.prototype.setPosition = function() {
   var styleSettings = {
+    top: this.top,
+    left: this.left,
     height: this.height,
     width: this.width
   };
--- src/jumpyDancer.js
+++ src/jumpyDancer.js
@@ -8,15 +8,25 @@ makeJumpyDancer.prototype.constructor = makeJumpyDancer;
 
 makeJumpyDancer.prototype.step = function(timeBetweenSteps) {
   setTimeout(()=> {
-    this.top += 20;
+    this.top += 10;
     // this.left += 20;
     this.setPosition();
-  }, 150);
+  }, 100);
   setTimeout(()=> {
-    // this.left -= 20;
-    this.top -= 20;
+    this.top += 10;
+    // this.left += 20;
+    this.setPosition();
+  }, 200);
+  setTimeout(()=> {
+    this.top -= 10;
+    // this.left += 20;
     this.setPosition();
   }, 300);
+  setTimeout(()=> {
+    // this.left -= 20;
+    this.top -= 10;
+    this.setPosition();
+  }, 400);
   setTimeout(() => { this.step(timeBetweenSteps) }, timeBetweenSteps);
 
   // this.$node.css("border", "10px solid pink");
